5992b8183255f1a5a52a6e9517b4514ed6d157ef
Andrey Gavrilov <boombardir21@gmail.com>
2022-11-11 12:14:55 +0500

    ref popup toggle

--- recipe/src/views/HomeView.vue
+++ recipe/src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
     <h1>My Recipes</h1>
-    <button @click="handler">Add new Recipe</button>
+    <button @click="togglePopup">Add new Recipe</button>
 
     <!-- Recipes -->
     <div class="recipes"></div>
-    <div class="add-recipe-popup">
+    <div class="add-recipe-popup" v-if="popupOpen">
       <div class="popup-content">
         <h2>Add new recipe</h2>
         <form @submit.prevent>
@@ -13,13 +13,25 @@
           <div class="group">
             <label>Description</label> <textarea></textarea>
           </div>
+
           <div class="group">
             <label>Ingredients</label>
             <div class="ingredient">
               <input type="text" />
-              <button>Remove</button>
             </div>
+            <button type="button">Add Ingredient</button>
           </div>
+
+          <div class="group">
+            <label>Methods</label>
+            <div class="method">
+              <textarea></textarea>
+            </div>
+            <button type="button">Add step</button>
+          </div>
+
+          <button type="submit">Add recipe</button>
+          <button type="button" @click="togglePopup">close</button>
         </form>
       </div>
     </div>
@@ -27,8 +39,108 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "HomeView",
   components: {},
+  setup() {
+    const newRecipe = ref({
+      title: "",
+      description: "",
+      ingredients: [],
+      methods: [],
+      ingredientsRows: 1,
+      methodsRows: 1,
+    });
+
+    const popupOpen = ref(false);
+
+    const togglePopup = () => {
+      popupOpen.value = !popupOpen.value;
+    };
+
+    return {
+      newRecipe,
+      togglePopup,
+      popupOpen,
+    };
+  },
 };
 </script>
+
+<style>
+.home {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+h1 {
+  font-size: 3rem;
+  margin-bottom: 32px;
+}
+.recipes {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+.recipes .card {
+  padding: 1rem;
+  border-radius: 5px;
+  margin: 1rem;
+  background-color: #081c33;
+}
+.recipes .card h2 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+.recipes .card p {
+  font-size: 1.125rem;
+  line-height: 1.4;
+  margin-bottom: 1rem;
+}
+.add-recipe-popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.add-recipe-popup .popup-content {
+  background-color: #081c33;
+  padding: 2rem;
+  border-radius: 1rem;
+  width: 100%;
+  max-width: 768px;
+}
+.popup-content h2 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+.popup-content .group {
+  margin-bottom: 1rem;
+}
+.popup-content .group label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+.popup-content .group input,
+.popup-content .group textarea {
+  display: block;
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+.popup-content .group textarea {
+  height: 100px;
+  resize: none;
+}
+.popup-content button[type="submit"] {
+  margin-right: 1rem;
+}
+</style>
